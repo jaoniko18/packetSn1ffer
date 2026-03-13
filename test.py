6cf2af8ce5c42aca44a354f9e672ed0b688ab75b
Nick <nickikun18@gmail.com>
2026-03-13 16:02:00 +0200

    simple sniffer is done!

--- test.py
+++ test.py
@@ -1,13 +1,33 @@
-"""
 from scapy.all import conf
 conf.use_pcap = True
 
 from scapy.all import sniff
 from packet_parser import PacketParser
+from scapy.layers.dns import DNS, DNSRR
 
+dns_cache = {}
 parser = PacketParser()
 
 def handle_packet(packet):
+    if packet.haslayer(DNS): #does packet has a dns layer?
+        dns = packet[DNS]
+        if dns.qr == 1: # response = 1, 0 = query
+            #extract name
+            for i in range(dns.ancount):
+                answer = dns.an[i]
+
+                if isinstance(answer, DNSRR):
+                    domain = answer.rrname.decode().rstrip('.')
+                    ip = answer.rdata #extract ip
+
+                    if isinstance(ip, str):
+                        dns_cache[ip] = domain #add to database(cache)
+                        print(f"DNS cached: {domain} -> {ip} ")
+                    elif isinstance(ip, list):
+                        for j in range(len(ip)):
+                            dns_cache[ip[j]] = domain
+                            print(f"DNS cached: {domain} -> {ip[j]} ")
+
     raw_packet = bytes(packet)
 
     # remove ethernet header
@@ -17,7 +37,17 @@ def handle_packet(packet):
         src_ip, dst_ip, protocol, header_length = parser.parse_ip_header(ip_packet)
         src_port, dst_port = parser.parse_transport_header(ip_packet)
 
-        print(protocol, src_ip, ":", src_port, "->", dst_ip, ":", dst_port)
+        if dst_ip in dns_cache:
+            domain_dst = dns_cache[dst_ip]
+        else:
+            domain_dst = dst_ip
+
+        if src_ip in dns_cache:
+            domain_src = dns_cache[src_ip]
+        else:
+            domain_src = src_ip
+
+        print(protocol, domain_src, ":", src_port, "->", domain_dst, ":", dst_port)
 
     except:
         pass
@@ -26,11 +56,3 @@ def handle_packet(packet):
 print("Sniffer started...")
 
 sniff(iface="en0", prn=handle_packet, store=False)
-"""
-from scapy.all import sniff
-
-def handle(packet):
-    if packet.haslayer("DNS"):
-        print(packet.summary())
-
-sniff(iface="en0", prn=handle, store=False)
